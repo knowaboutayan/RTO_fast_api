e48a342ce965cb3414763f973884237a87833ba4
knowaboutayan <info.creationology@gmail.com>
2024-08-17 19:07:39 +0530

    hh

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://rto-managemet-system.vercel.app/"],  # Or use ["*"] to allow all origins
+    allow_origins=["http://localhost:5173","https://rto-managemet-system.vercel.app"],  # Or use ["*"] to allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
